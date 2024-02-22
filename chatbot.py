8cb63acf384e04e27fa591f34d0794d3d5a93dc1
chongjiu <justinagao9@gmail.com>
2024-02-22 00:11:59 -0800

    use the newest version of openAI API

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,5 @@
-import os
-from dotenv import load_dotenv
-import openai
-
-load_dotenv()
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-completion = openai.ChatCompletion()
+from openai import OpenAI
+client = OpenAI()
 
 start_chat_log = [
     {"role": "system", "content": "You are a helpful assistant."},
@@ -14,9 +9,15 @@ def askgpt(question, chat_log=None):
     if chat_log is None:
         chat_log = start_chat_log
     chat_log = chat_log + [{'role': 'user', 'content': question}]
-    response = completion.create(model='gpt-3.5-turbo', messages=chat_log)
-    answer = response.choices[0]['message']['content']
+    
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log
+    )
+    
+    answer = completion.choices[0].message.content
     chat_log = chat_log + [{'role': 'assistant', 'content': answer}]
+    
     return answer, chat_log
   
 
