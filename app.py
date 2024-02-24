ec308823ca96d781222f8b123cdfba31a792d177
chongjiu <justinagao9@gmail.com>
2024-02-23 16:25:48 -0800

    add fake onboarding message

--- app.py
+++ app.py
@@ -5,14 +5,28 @@ from chatbot import askgpt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
 
+# Fake database that stores user contact information
+Users_db = {}
+
+welcome_msg="Hello there! I'm Linda, your personalized AI assistant. What's your name?"
+
 
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
+    
+    # Special prompt for new user
+    user_info = request.form['From']
+    if Users_db.get(user_info) is None:
+        Users_db[user_info] = True
+        r = MessagingResponse()
+        r.message(welcome_msg)
+        return str(r)
+    
     chat_log = session.get('chat_log')
-
     answer, chat_log = askgpt(incoming_msg, chat_log)
     session['chat_log'] = chat_log
+    
 
     r = MessagingResponse()
     r.message(answer)
